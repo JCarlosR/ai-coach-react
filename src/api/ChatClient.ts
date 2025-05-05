1b450b0e62603fd6d821fac26ba2c5be580dd349
Juan Ramos <juancagb.17@gmail.com>
2025-05-05 10:06:00 -0500

    Add env variable to override on Prod

--- src/api/ChatClient.ts
+++ src/api/ChatClient.ts
@@ -3,7 +3,7 @@ import { Message } from './types';
 export class ChatClient {
   private baseUrl: string;
 
-  constructor(baseUrl: string = 'http://localhost:8000') {
+  constructor(baseUrl: string = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000') {
     this.baseUrl = baseUrl;
   }
 
